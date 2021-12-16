76c7b9a948f6f62a60dd86a8c721d320dc16b746
Ghulam Yaseen <gy@agifly.be>
2021-12-16 18:06:59 +0500

    change variablein tabs

--- resources/views/petitions/_partials/tabs.blade.php
+++ resources/views/petitions/_partials/tabs.blade.php
@@ -1,8 +1,8 @@
       <ul class="nav nav-tabs mb-5">
           <li class="nav-item">
-              <a class="nav-link {{isset($active_app_view)?'active':''}}" href="{{ route('petitions.edit', $record->id) }}">Basic</a>
+              <a class="nav-link {{isset($active_app_view)?'active':''}}" href="{{ route('petitions.edit', $petition_id) }}">Basic</a>
           </li>
           <li class="nav-item">
-              <a class="nav-link {{isset($petition_documents_view)?'active':''}}" href="{{ url('petition_documents/create?petition_id='.$record->id) }}">Documents</a>
+              <a class="nav-link {{isset($petition_documents_view)?'active':''}}" href="{{ url('petition_documents/create?petition_id='.$petition_id) }}">Documents</a>
           </li>
       </ul> 
