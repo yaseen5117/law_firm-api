bede542d52767d89b19b1004b5a15cf420f27657
Ghulam Yaseen <gy@agifly.be>
2021-12-28 15:33:02 +0500

    create petition clients table

---
+++ app/Models/PetitionClient.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PetitionClient extends Model
+{
+    use HasFactory;
+}
