3c557e28dbff2c5acb69655b43e76070429edbe6
Ghulam Yaseen <gy@agifly.be>
2022-02-25 11:26:18 +0500

    attachments controller changes

--- app/Http/Controllers/Api/AttachmentController.php
+++ app/Http/Controllers/Api/AttachmentController.php
@@ -35,27 +35,31 @@ class AttachmentController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {
-        $request->validate([
-            'file' => 'required|mimes:jpg,jpeg,png' //csv,txt,xlx,xls,pdf
-         ]);
-         
-         $fileUpload = new Attachment();
-         if($request->file()) {
-            $name = time().'_'.$request->file->getClientOriginalName();
-            $file_path = $request->file('file')->storeAs('attachments/'.$request->attachmentable_id, $name, 'public');
- 
-            $fileUpload->file_name = time().'_'.$request->file->getClientOriginalName();
-            //$fileUpload->path = '/storage/' . $file_path;
-            $fileUpload->attachmentable_type = 'App\Models\PetitionIndex';
-            $fileUpload->attachmentable_id = $request->attachmentable_id;
-            $fileUpload->mime_type = $request->file->getClientMimeType();
-            $fileUpload->save();
+    {         
+        try {
+            $fileUpload = new Attachment();
+            if ($request->file()) {
+                $name = time() . '_' . $request->file->getClientOriginalName();
+                $file_path = $request->file('file')->storeAs('attachments/' . $request->attachmentable_id, $name, 'public');
 
-            return response()->json([
-                'success'=>'File uploaded successfully.',
-                'file_data' => $fileUpload,
-            ]);
+                $fileUpload->file_name = time() . '_' . $request->file->getClientOriginalName();
+                $fileUpload->title = $fileUpload->file_name;
+                //$fileUpload->path = '/storage/' . $file_path;
+                $fileUpload->attachmentable_type = 'App\Models\PetitionIndex';
+                $fileUpload->attachmentable_id = $request->attachmentable_id;
+                $fileUpload->mime_type = $request->file->getClientMimeType();
+                $fileUpload->save();
+
+                return response()->json([
+                    'success' => 'File uploaded successfully.',
+                    'file_data' => $fileUpload,
+                    'code' => 200,
+                ]);
+            }
+        } catch (\Exception $e) {
+            return response([
+                "error" => $e->getMessage()
+            ], 500);
         }
     }
 
@@ -90,7 +94,26 @@ class AttachmentController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        try {
+            $attachment = Attachment::find($id);
+            if ($attachment) {
+                $attachment->update([
+                    'title' => $request->title,
+                    'display_order' => $request->display_order,
+                ]);
+                return response(
+                    [
+                        'message' => 'Record updated successfully',
+                        'attachment' => $attachment,
+                        'code' => 200
+                    ]
+                );
+            }
+        } catch (\Exception $e) {
+            return response([
+                "error" => $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -101,6 +124,21 @@ class AttachmentController extends Controller
      */
     public function destroy($id)
     {
-        //
+        try {
+            $attachment = Attachment::find($id);
+            if ($attachment) {
+                $attachment->delete();
+                return response(
+                    [
+                        'message' => 'Record Deleted successfully',    
+                        'code' => 200                    
+                    ]
+                );
+            }
+        } catch (\Exception $e) {
+            return response([
+                "error" => $e->getMessage()
+            ], 500);
+        }
     }
 }
