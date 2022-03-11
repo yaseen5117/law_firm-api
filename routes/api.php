1c34f682114af3f699b93a192161bf7285037492
Hamza <imhamza@outlook.com>
2022-03-11 17:23:58 +0500

    updates

--- routes/api.php
+++ routes/api.php
@@ -27,6 +27,7 @@ use Illuminate\Support\Facades\Route;
     Route::resource('courts', 'Api\CourtController');
     
     Route::resource('petition_replies', 'Api\PetitionReplyController');
+    Route::resource('petition_order_sheets', 'Api\PetitionOrderSheetController');
     //Route::resource('petition_indexes', 'Api\PetitionIndexController');
     //Route::resource('petitions', 'Api\PetitionController');
     
