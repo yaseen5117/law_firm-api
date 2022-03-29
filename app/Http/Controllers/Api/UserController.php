4f0fa48feff203363188c3fff1d9a80cb38c4df1
Ghulam Yaseen <gy@agifly.be>
2022-03-29 18:36:03 +0500

    add function to except of middlware in usercontroller

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
 
     public function __construct()
     {    
-        $this->middleware("role:admin", ['except' => ['store', 'show','index','getClient','getLoggedInUser','getRoles']]);      
+        $this->middleware("role:admin", ['except' => ['store', 'show','index','getClient','getLoggedInUser','getRoles','signUp']]);      
     }
     /**
      * Display a listing of the resource.
