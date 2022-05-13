15ccff66737a3e77a98dcec46504d0d45e18851a
Ghulam Yaseen <ghulamyaseenmalik206@gmail.com>
2022-05-13 13:46:54 +0500

    download invoice

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
         Route::resource('users', 'UserController');             
         Route::resource('petition_hearing', 'PetitionHearingController');
         Route::resource('invoices', 'InvoiceController'); 
-        Route::get('download_pdf', 'InvoiceController@downloadInvoicePdf');      
+        Route::get('download_pdf/{id}', 'InvoiceController@downloadInvoicePdf');      
         
     });
  
