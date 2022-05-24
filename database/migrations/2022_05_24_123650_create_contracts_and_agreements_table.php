be6f4fb8d9b360b7e839d1240699c27cc316a357
Ghulam Yaseen <ghulamyaseenmalik206@gmail.com>
2022-05-24 15:29:06 +0500

    create model and table contract and agreement

---
+++ database/migrations/2022_05_24_123650_create_contracts_and_agreements_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContractsAndAgreementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contracts_and_agreements', function (Blueprint $table) {
+            $table->id();          
+            $table->unsignedBigInteger('contract_category_id')->nullable();  
+            $table->string('title')->nullable();       
+            $table->integer('display_order')->nullable();      
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contracts_and_agreements');
+    }
+}
