22512b7b54179f1a7154adc7e96c915a72bb51ff
Ghulam Yaseen <ghulamyaseenmalik206@gmail.com>
2022-06-13 15:26:23 +0500

    add all routes inside mddleware

--- routes/api.php
+++ routes/api.php
@@ -13,123 +13,128 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-    Route::get('test_convert', 'Api\PetitionController@test_convert');
+Route::get('test_convert', 'Api\PetitionController@test_convert');
+
+Route::group(['middleware' => 'auth:sanctum', 'namespace' => 'Api'], function () {
+
+    Route::post('petitions/toggle_archived', 'PetitionController@toggleArchivedStatus');
+    Route::resource('petitions', 'PetitionController');
+    Route::resource('users', 'UserController');
+    Route::resource('petition_hearing', 'PetitionHearingController');
+    Route::get('invoices/stats', 'InvoiceController@invoicesStats');
+    Route::resource('invoices', 'InvoiceController');
+    //Route::post('invoices/mark_paid', 'InvoiceController@mark_paid'); 
+    Route::get('download_pdf/{id}', 'InvoiceController@downloadInvoicePdf');
+    Route::delete('delete_invoice_expense/{invoice_expense_id}', 'InvoiceController@deleteInvoiceExpense');
+    Route::get('invoice_statuses', 'InvoiceController@invoice_statuses');
+    Route::get('invoice_templates', 'InvoiceController@invoice_templates');
+    Route::post('invoice/mark_as_paid', 'InvoiceController@markAsPaid');
+    Route::resource('contracts_and_agreements', 'ContractsAndAgreementController');
+    Route::resource('links', 'LinkController');
+    Route::get('contract_categories', 'ContractsAndAgreementController@contractCategory');
+    Route::resource('settings', 'SettingController');
+    Route::get('get_order_sheet_types', 'PetitionOrderSheetController@getOrderSheetType');
+    Route::post('save_order_sheet_types', 'PetitionOrderSheetController@saveOrderSheetType');
+    Route::delete('delete_order_sheet_type/{id}', 'PetitionOrderSheetController@deleteOrderSheet');
+    //Route::post('delete_additional_email','SettingController@deleteAdditionalEmail');
     
-    Route::group(['middleware' => 'auth:sanctum','namespace' => 'Api'], function () { 
-  
-        Route::post('petitions/toggle_archived', 'PetitionController@toggleArchivedStatus');
-        Route::resource('petitions', 'PetitionController');
-        Route::resource('users', 'UserController');             
-        Route::resource('petition_hearing', 'PetitionHearingController');
-        Route::get('invoices/stats', 'InvoiceController@invoicesStats'); 
-        Route::resource('invoices', 'InvoiceController'); 
-        //Route::post('invoices/mark_paid', 'InvoiceController@mark_paid'); 
-        Route::get('download_pdf/{id}', 'InvoiceController@downloadInvoicePdf');     
-        Route::delete('delete_invoice_expense/{invoice_expense_id}', 'InvoiceController@deleteInvoiceExpense'); 
-        Route::get('invoice_statuses', 'InvoiceController@invoice_statuses'); 
-        Route::get('invoice_templates', 'InvoiceController@invoice_templates'); 
-        Route::post('invoice/mark_as_paid','InvoiceController@markAsPaid');
-        Route::resource('contracts_and_agreements', 'ContractsAndAgreementController');
-        Route::resource('links', 'LinkController');
-        Route::get('contract_categories', 'ContractsAndAgreementController@contractCategory');
-        Route::resource('settings', 'SettingController');
-        Route::get('get_order_sheet_types', 'PetitionOrderSheetController@getOrderSheetType');
-        Route::post('save_order_sheet_types', 'PetitionOrderSheetController@saveOrderSheetType');
-        Route::delete('delete_order_sheet_type/{id}', 'PetitionOrderSheetController@deleteOrderSheet');
-        //Route::post('delete_additional_email','SettingController@deleteAdditionalEmail');
-    });
- 
-    // Public routes
-    Route::post('register', 'AuthController@register');
-    Route::post('login', 'AuthController@login');
-    Route::post('roles', 'Api\UserController@getRoles');
-    Route::post('signup', 'Api\UserController@signUp');
-    Route::get('clients', 'Api\UserController@getClient');
-    Route::get('client_users', 'Api\UserController@getClientUsers');
-    Route::get('lawyers', 'Api\UserController@getLawyer'); 
-    Route::post('delete_folder', 'Api\AttachmentController@findOriginalFolder');
-  
-    
+    Route::get('clients', 'UserController@getClient');
+    Route::get('client_users', 'UserController@getClientUsers');
+    Route::get('lawyers', 'UserController@getLawyer');
+    Route::post('delete_folder', 'AttachmentController@findOriginalFolder');
+
+
     //contact request
-    Route::post('contact_requests', 'Api\FrontEndController@contactRequest');  
-    Route::get('get_contact_requests', 'Api\FrontEndController@getContactRequest');
-    Route::post('delete_contact_request/{id}', 'Api\FrontEndController@deleteContactRequest');
+    Route::post('contact_requests', 'FrontEndController@contactRequest');
+    Route::get('get_contact_requests', 'FrontEndController@getContactRequest');
     //opinions
-    Route::resource('opinions', 'Api\OpinionController');
-    Route::resource('attachments', 'Api\AttachmentController');
-
-    Route::post('delete_selected', 'Api\AttachmentController@deleteSelected');
+    Route::resource('opinions', 'OpinionController');
     
-    Route::resource('petition_types', 'Api\PetitionTypeController');
-    Route::resource('courts', 'Api\CourtController');
-    
-    Route::resource('petition_replies', 'Api\PetitionReplyController');
 
-    Route::resource('petition_order_sheets', 'Api\PetitionOrderSheetController');
-    Route::POST('petition_order_sheets/by_petition', 'Api\PetitionOrderSheetController@showOrderSheetByPetition');
+    Route::post('delete_selected', 'AttachmentController@deleteSelected');
+
+    Route::resource('petition_types', 'PetitionTypeController');
+    Route::resource('courts', 'CourtController');
+
+    Route::resource('petition_replies', 'PetitionReplyController');
+
+   
+    Route::POST('petition_order_sheets/by_petition', 'PetitionOrderSheetController@showOrderSheetByPetition');
     //START route for Talbana forms
-    Route::resource('petition_talbana', 'Api\PetitionTalbanaController');
-    Route::POST('petition_talbana/by_petition', 'Api\PetitionTalbanaController@showTalbanaByPetition');
+    Route::resource('petition_talbana', 'PetitionTalbanaController');
+    Route::POST('petition_talbana/by_petition', 'PetitionTalbanaController@showTalbanaByPetition');
     //END route for Talbana forms
 
     //START route for Petition Naqal Form
-    Route::resource('petition_naqal_forms', 'Api\NaqalFormController');
-    Route::POST('petition_naqal_forms/by_petition', 'Api\NaqalFormController@showNaqalFormByPetition');
+    Route::resource('petition_naqal_forms', 'NaqalFormController');
+    Route::POST('petition_naqal_forms/by_petition', 'NaqalFormController@showNaqalFormByPetition');
     //END route for Petition Naqal Form
 
-    Route::post('petition_reply_details/{id}', 'Api\PetitionReplyController@replyDetail');
-    Route::resource('petition_reply_parents', 'Api\PetitionReplyParentController');
     
+    Route::resource('petition_reply_parents', 'PetitionReplyParentController');
+
     //START route for standard page Oral Arguments
-    Route::resource('oral_arguments', 'Api\OralArgumentsController');
-    Route::post('module_index_details_oral_arguments/{id}', 'Api\OralArgumentsController@oralArgumentDetail');
+    Route::resource('oral_arguments', 'OralArgumentsController');    
     //END route for standard page Oral Arguments
 
     //START route for standard page Case Law
-    Route::resource('case_laws', 'Api\CaseLawController');
-    Route::post('module_index_details_case_laws/{id}', 'Api\CaseLawController@caseLawDetail');
+    Route::resource('case_laws', 'CaseLawController');    
     //END route for standard page Case Law
 
     //START route for standard page Extra Document
-    Route::resource('extra_documents', 'Api\ExtraDocumentController');
-    Route::post('module_index_details_extra_documents/{id}', 'Api\ExtraDocumentController@extraDocsDetail');
+    Route::resource('extra_documents', 'ExtraDocumentController');    
     //END route for standard page Extra Document
 
     //START route for standard page Judgement
-    Route::resource('judgements', 'Api\JudgementController');
-    Route::post('module_index_details_judgements/{id}', 'Api\JudgementController@judgementDetail');
+    Route::resource('judgements', 'JudgementController');    
     //END route for standard page Judgement
 
     //START route for Synopses forms
-    Route::resource('petition_synopsis', 'Api\PetitionSynopsisController');
-    Route::POST('petition_synopsis/by_petition', 'Api\PetitionSynopsisController@showSynopsisByPetition');
+    Route::resource('petition_synopsis', 'PetitionSynopsisController');
+    Route::POST('petition_synopsis/by_petition', 'PetitionSynopsisController@showSynopsisByPetition');
     //END route for Synopses forms
 
     //START route for Synopses forms
-    Route::resource('general_case_laws', 'Api\GeneralCaseLawController');
+    Route::resource('general_case_laws', 'GeneralCaseLawController');
     //END route for Synopses forms
 
-    //Route::resource('petition_indexes', 'Api\PetitionIndexController');
-    //Route::resource('petitions', 'Api\PetitionController');
-    
-    Route::resource('petitions_index', 'Api\PetitionIndexController');
-    Route::middleware('auth:sanctum')->get('/user', 'Api\UserController@getLoggedInUser');
-    Route::group(['middleware' => 'auth:sanctum','namespace' => 'Api'], function () { 
-  
-        
-        // Route::resource('admins', AdminController::class);  
-        // Route::put('company/update/{id}', 'CompanyController@update');
-        // Route::get('company/{id}', 'CompanyController@show');
-        // Route::get('customer', 'CustomerController@index');
-        // Route::post('customer/store', 'CustomerController@store');
-        // Route::put('customer/update/{id}', 'CustomerController@update');
-        // Route::get('customer/{id}', 'CustomerController@show');
-        // Route::get('product', 'ProductController@index');
-        // Route::post('product/store', 'ProductController@store');
-        // Route::put('product/update/{id}', 'ProductController@update');
-        // Route::get('product/{id}', 'ProductController@show');
-        // Route::post('logout', 'CustomerController@logout');
-    });
+    //Route::resource('petition_indexes', 'PetitionIndexController');
+    //Route::resource('petitions', 'PetitionController');
+
+    Route::resource('petitions_index', 'PetitionIndexController');
+});
+
+Route::post('delete_contact_request/{id}', 'Api\FrontEndController@deleteContactRequest');
+Route::post('roles', 'Api\UserController@getRoles');
+Route::post('signup', 'Api\UserController@signUp');
+Route::post('module_index_details_judgements/{id}', 'Api\JudgementController@judgementDetail');
+Route::post('module_index_details_extra_documents/{id}', 'Api\ExtraDocumentController@extraDocsDetail');
+Route::post('module_index_details_case_laws/{id}', 'Api\CaseLawController@caseLawDetail');
+Route::resource('attachments', 'Api\AttachmentController');
+Route::post('module_index_details_oral_arguments/{id}', 'Api\OralArgumentsController@oralArgumentDetail');
+Route::resource('petition_order_sheets', 'Api\PetitionOrderSheetController');
+Route::post('petition_reply_details/{id}', 'Api\PetitionReplyController@replyDetail');
+// Public routes
+Route::post('register', 'AuthController@register');
+Route::post('login', 'AuthController@login');
+
+Route::middleware('auth:sanctum')->get('/user', 'Api\UserController@getLoggedInUser');
+Route::group(['middleware' => 'auth:sanctum', 'namespace' => 'Api'], function () {
+
+
+    // Route::resource('admins', AdminController::class);  
+    // Route::put('company/update/{id}', 'CompanyController@update');
+    // Route::get('company/{id}', 'CompanyController@show');
+    // Route::get('customer', 'CustomerController@index');
+    // Route::post('customer/store', 'CustomerController@store');
+    // Route::put('customer/update/{id}', 'CustomerController@update');
+    // Route::get('customer/{id}', 'CustomerController@show');
+    // Route::get('product', 'ProductController@index');
+    // Route::post('product/store', 'ProductController@store');
+    // Route::put('product/update/{id}', 'ProductController@update');
+    // Route::get('product/{id}', 'ProductController@show');
+    // Route::post('logout', 'CustomerController@logout');
+});
  
 // // Public routes
 // Route::post('register', 'AuthController@register');
@@ -208,4 +213,3 @@ use Illuminate\Support\Facades\Route;
 //     // Route::get('product/{id}', 'ProductController@show');
 //     // Route::post('logout', 'CustomerController@logout');
 // });
- 
