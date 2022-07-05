899d103b2c35e7e37c4f1d5b05d55c704cce0b7c
Hamza Aslam <imhamza@outlook.com>
2022-07-05 17:24:47 +0500

    log removed

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,8 +25,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('inspire')->hourly();
-        info("scheduler is running");
+        //$schedule->command('inspire')->hourly();
+        //info("scheduler is running");
 
     }
 
