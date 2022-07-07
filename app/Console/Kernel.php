4df8836caaa09f3baf3175ae4892205acbd28288
Hamza Aslam <imhamza@outlook.com>
2022-07-07 13:22:48 +0500

    scheduler set for hearning reminder

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        \App\Console\Commands\SendEmailBeforeHearing::class,
+        //\App\Console\Commands\SendEmailBeforeHearing::class,
     ];
 
     /**
