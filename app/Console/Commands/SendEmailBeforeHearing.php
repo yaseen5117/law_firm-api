6bfb74afb5f468672820371b75bbb5f501978548
Hamza Aslam <imhamza@outlook.com>
2022-07-07 12:48:32 +0500

    scheduler set for hearning reminder

---
+++ app/Console/Commands/SendEmailBeforeHearing.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Http\Controllers\Api\CronjobController;
+class SendEmailBeforeHearing extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'reminder:send-email-before-hearing';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This command will send an email 1 day before hearning at 12 pm.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $cronjobController = new CronjobController;
+        $cronjobController->send_email_before_hearing();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        return 0;
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        \App\Console\Commands\SendEmailBeforeHearing::class,
     ];
 
     /**
@@ -27,6 +27,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         //$schedule->command('inspire')->hourly();
         //info("scheduler is running");
+        $schedule->command('reminder:send-email-before-hearing')->everyMinute();
 
     }
 
