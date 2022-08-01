cb466c2c37122ee9b083bf061dfa6baedbae00c1
Hamza Aslam <imhamza@outlook.com>
2022-08-01 11:39:13 +0500

    send email without job

--- app/Http/Controllers/Api/AttachmentController.php
+++ app/Http/Controllers/Api/AttachmentController.php
@@ -155,11 +155,83 @@ class AttachmentController extends Controller
                 }
 
                 //SENDING EMAIL VIA QUEUE JOB
-                /*$sendDocumentUploadEmail = new SendDocumentUploadEmail($attachmentable_type,$attachmentable_id);
-                $sendDocumentUploadEmail->dispatch();*/
+                //$job = (new SendDocumentUploadEmail($attachmentable_type, $attachmentable_id))->delay(Carbon::now()->addSeconds(40));
+                //$this->dispatch($job);
 
-                $job = (new SendDocumentUploadEmail($attachmentable_type, $attachmentable_id))->delay(Carbon::now()->addSeconds(40));
-                $this->dispatch($job);
+                //SENDING EMAIL VIA email service
+                switch ($attachmentable_type) {
+                    case 'App\Models\PetitonOrderSheet':
+                        $entity_title = "Order Sheet";
+                        $pettiion_ordersheet = PetitonOrderSheet::find($attachmentable_id);
+                        $petition = $pettiion_ordersheet->petition;
+                        break;
+
+                    case 'App\Models\PetitionIndex':
+                        //id22
+                        $entity_title = "Petition Index";
+                        $petition_index = PetitionIndex::find($attachmentable_id);
+                        $petition = $petition_index->petition;
+                        break;
+
+                    case 'App\Models\PetitionReply':
+                        $entity_title = "Replies";
+                        $petition_reply = PetitionReply::find($attachmentable_id);
+                        $petition = $petition_reply->petition_reply_parent->petition;
+                        break;
+
+                    case 'App\Models\OralArgument':
+                        $entity_title = "Oral Argument";
+                        $petition_oral_argument = OralArgument::find($attachmentable_id);
+                        $petition = $petition_oral_argument->petition;
+                        break;
+
+                    case 'App\Models\PetitionNaqalForm':
+                        $entity_title = "Naqal Form";
+                        $petition_naqal_form = PetitionNaqalForm::find($attachmentable_id);
+                        $petition = $petition_naqal_form->petition;
+                        break;
+
+                    case 'App\Models\PetitionTalbana':
+                        $entity_title = "Talbana";
+                        $petition_talbana = PetitionTalbana::find($attachmentable_id);
+                        $petition = $petition_talbana->petition;
+                        break;
+
+                    case 'App\Models\CaseLaw':
+                        $entity_title = "Case Laws";
+                        $petition_case_law = CaseLaw::find($attachmentable_id);
+                        $petition = $petition_case_law->petition;
+                        break;
+
+                    case 'App\Models\ExtraDocument':
+                        $entity_title = "Extra Document";
+                        $petition_extra_document = ExtraDocument::find($attachmentable_id);
+                        $petition = $petition_extra_document->petition;
+                        break;
+
+                    case 'App\Models\PetitionSynopsis':
+                        $entity_title = "Synopsis";
+                        $petition_synopsis = PetitionSynopsis::find($attachmentable_id);
+                        $petition = $petition_synopsis->petition;
+                        break;
+
+                    case 'App\Models\Judgement':
+                        $entity_title = "Judgement";
+                        $petition_judgement = Judgement::find($attachmentable_id);
+                        $petition = $petition_judgement->petition;
+                        break;
+
+                    default:
+                        $entity_title = "";
+                        break;
+                }
+
+                if (isset($petition) && !empty($entity_title)) {
+                    info("SendDocumentUploadEmail queue job sending email to petition id: $petition->id for entity_title $entity_title");
+                    $emailService = new EmailService;
+                    $emailService->send_document_uploading_email($petition, $entity_title);
+                }
+                
 
 
                 info('--------end ATTCHMENT UPLOADING PROCESS--------');
