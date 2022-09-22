cfb96395b69453ab48c002a43f797d9e0c56a77d
Hamza Aslam <imhamza@outlook.com>
2022-09-22 16:58:54 +0500

    logic fix

--- app/Http/Controllers/Api/SettingController.php
+++ app/Http/Controllers/Api/SettingController.php
@@ -18,7 +18,7 @@ class SettingController extends Controller
     {
         try {
 
-            $request_domain = \Request::root();
+            $request_domain = \Request::server('HTTP_REFERER');
             info("Request domain $request_domain");
             //logged in User
             $user = request()->user();
@@ -28,7 +28,7 @@ class SettingController extends Controller
                 $setting = Setting::find($user->company_id)->getMeta()->toArray();
             }else{
                 
-                $allowed_domains = ["http://localhost:8000","https://localhost:8000","https://elawfirmpk.com"];
+                $allowed_domains = ["http://localhost:8080/","https://elawfirmpk.com/"];
 
                 if (in_array($request_domain, $allowed_domains)) {
                     $company = Company::where('id',1)->first();
