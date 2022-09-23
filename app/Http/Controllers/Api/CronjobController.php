3645fb3f4e0395c7664847c1d07c7b49c6a0fc3b
Ghulam Yaseen <ghulamyaseenmalik206@gmail.com>
2022-09-23 15:16:59 +0500

    format code

--- app/Http/Controllers/Api/CronjobController.php
+++ app/Http/Controllers/Api/CronjobController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Api;
+
 use App\Services\EmailService;
 use App\Models\PetitionHearing;
 use App\Http\Controllers\Controller;
@@ -13,22 +14,19 @@ class CronjobController extends Controller
 
     function __construct()
     {
-        
-        
     }
 
     public function send_email_before_hearing()
     {
         $emailService = new EmailService;
-        info("CronjobController: send_email_before_hearing: START at ". date("d-M-Y h:i:s a"));
+        info("CronjobController: send_email_before_hearing: START at " . date("d-M-Y h:i:s a"));
 
         $date_tomorrow = Date("Y-m-d", strtotime("+1 day"));
-        $hearings_tomorrow = PetitionHearing::where('hearing_date',$date_tomorrow)->get();
+        $hearings_tomorrow = PetitionHearing::where('hearing_date', $date_tomorrow)->get();
         foreach ($hearings_tomorrow as $tomorrow_hearing) {
             $emailService->send_email_before_hearing($tomorrow_hearing);
         }
-        info("CronjobController: send_email_before_hearing: Hearings found for date $date_tomorrow are: ". $hearings_tomorrow->count(). " Hearing IDs ".print_r($hearings_tomorrow->pluck('id')->all(),1));
-        info("CronjobController: send_email_before_hearing: END at ". date("d-M-Y h:i:s a"));
-        
+        info("CronjobController: send_email_before_hearing: Hearings found for date $date_tomorrow are: " . $hearings_tomorrow->count() . " Hearing IDs " . print_r($hearings_tomorrow->pluck('id')->all(), 1));
+        info("CronjobController: send_email_before_hearing: END at " . date("d-M-Y h:i:s a"));
     }
 }
