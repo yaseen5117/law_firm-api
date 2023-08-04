a8e5c9ba0d7a8779a5117102e70e31c85a98e93d
Hamza Aslam <imhamzaaslam@gmail.com>
2023-08-04 11:08:31 +0500

    pdf service class added

---
+++ app/Services/PdfService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Setting;
+use Exception;
+
+class PdfService
+{
+
+    function __construct()
+    {
+    }
+
+    /**
+     * This function gets PDF from storage and convert it to images.
+     *
+     *
+     * @param Type $var Description
+     * @return type
+     * @throws conditon
+     **/
+    public function convertPdftoimages(Type $var = null17)
+    {
+        # code...
+    }
+
+
+    /**
+     * This function gets images from storage and convert it to PDF.
+     *
+     *
+     * @param Type $var Description
+     * @return type
+     * @throws conditon
+     **/
+    public function convertImagesToPdf(Type $var = null17)
+    {
+        # code...
+    }
+}
