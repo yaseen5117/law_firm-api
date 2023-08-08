9214d2438d62a9f022fa4545a9e26ba282eec30c
Ghulam Yaseen <ghulamyaseenmalik206@gmail.com>
2023-08-08 16:21:52 +0500

    add html, body

--- resources/views/petition_pdf/download_index_images_as_pdf.blade.php
+++ resources/views/petition_pdf/download_index_images_as_pdf.blade.php
@@ -1,12 +1,22 @@
-<style>
+<!DOCTYPE html>
+<html>
 
-</style>
+<head>
+    <title></title>
+    <!-- Additional meta tags, stylesheets, scripts, etc. -->
+</head>
 
-@foreach(@$attachments as $attachment)
-@if($attachment)
-<div>
-    <img class="image_style" width="100%" src="data:image/jpeg;base64,{{ base64_encode(file_get_contents(public_path($file_path . $attachment['file_name']))) }}" alt="Image">
-    <br><br>
-</div>
-@endif
-@endforeach
+<body>
+    <div>
+        @foreach(@$attachments as $attachment)
+            @if($attachment)
+            <div>
+                <img class="image_style" width="100%" src="data:image/jpeg;base64,{{ base64_encode(file_get_contents(public_path($file_path . $attachment['file_name']))) }}" alt="Image">
+                <br><br>
+            </div>
+            @endif
+        @endforeach
+    </div>
+</body>
+
+</html>
