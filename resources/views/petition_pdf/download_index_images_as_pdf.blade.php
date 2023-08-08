1f7301f19c158e86b76baba3c68003d911b9a8f5
Ghulam Yaseen <ghulamyaseenmalik206@gmail.com>
2023-08-08 16:14:25 +0500

    change div

--- resources/views/petition_pdf/download_index_images_as_pdf.blade.php
+++ resources/views/petition_pdf/download_index_images_as_pdf.blade.php
@@ -2,15 +2,11 @@
 
 </style>
 
+@foreach(@$attachments as $attachment)
+@if($attachment)
 <div>
-
-    @foreach(@$attachments as $attachment)
-    @if($attachment)
-    <div>
-        <img class="image_style" width="100%" src="data:image/jpeg;base64,{{ base64_encode(file_get_contents(public_path($file_path . $attachment['file_name']))) }}" alt="Image">
-        <br><br>
-    </div>
-    @endif
-    @endforeach
-
+    <img class="image_style" width="100%" src="data:image/jpeg;base64,{{ base64_encode(file_get_contents(public_path($file_path . $attachment['file_name']))) }}" alt="Image">
+    <br><br>
 </div>
+@endif
+@endforeach
