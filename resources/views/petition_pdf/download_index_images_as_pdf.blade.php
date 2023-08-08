366ed4a511827c3f7829ef9f6c918ed77a633616
Ghulam Yaseen <ghulamyaseenmalik206@gmail.com>
2023-08-08 16:40:45 +0500

    remove br tag

--- resources/views/petition_pdf/download_index_images_as_pdf.blade.php
+++ resources/views/petition_pdf/download_index_images_as_pdf.blade.php
@@ -11,8 +11,7 @@
         @foreach(@$attachments as $attachment)
             @if($attachment)
             <div>
-                <img class="image_style" width="100%" src="data:image/jpeg;base64,{{ base64_encode(file_get_contents(public_path($file_path . $attachment['file_name']))) }}" alt="Image">
-                <br><br>
+                <img class="image_style" width="100%" src="data:image/jpeg;base64,{{ base64_encode(file_get_contents(public_path($file_path . $attachment['file_name']))) }}" alt="Image">                 
             </div>
             @endif
         @endforeach
