3dbd7e55a692575e9d2e732d5c832a7029529788
Ghulam Yaseen <ghulamyaseenmalik206@gmail.com>
2023-08-08 16:42:46 +0500

    remove div

--- resources/views/petition_pdf/download_index_images_as_pdf.blade.php
+++ resources/views/petition_pdf/download_index_images_as_pdf.blade.php
@@ -7,15 +7,11 @@
 </head>
 
 <body>
-    <div>
-        @foreach(@$attachments as $attachment)
-            @if($attachment)
-            <div>
-                <img class="image_style" width="100%" src="data:image/jpeg;base64,{{ base64_encode(file_get_contents(public_path($file_path . $attachment['file_name']))) }}" alt="Image">                 
-            </div>
-            @endif
-        @endforeach
-    </div>
+    @foreach(@$attachments as $attachment)
+        @if($attachment)
+        <img class="image_style" width="100%" src="data:image/jpeg;base64,{{ base64_encode(file_get_contents(public_path($file_path . $attachment['file_name']))) }}" alt="Image">
+        @endif
+    @endforeach
 </body>
 
 </html>
