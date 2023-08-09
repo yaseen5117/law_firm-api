6f8acd0944559932a17b8610e54cf052c6e92462
Ghulam Yaseen <ghulamyaseenmalik206@gmail.com>
2023-08-09 14:47:08 +0500

    change

--- resources/views/petition_pdf/download_index_images_as_pdf.blade.php
+++ resources/views/petition_pdf/download_index_images_as_pdf.blade.php
@@ -4,14 +4,40 @@
 <head>
     <title></title>
     <!-- Additional meta tags, stylesheets, scripts, etc. -->
+
+    <style>
+        .page-break {
+            page-break-after: always;
+        }
+    </style>
 </head>
 
 <body>
-    @foreach(@$attachments as $attachment)
-        @if($attachment)
+    <table class="content">
+        <thead>
+            <tr>
+                <th class="td">Description of Documents</th>
+                <th class="td">Date</th>
+                <th class="td">Annexure</th>
+                <th class="td">Page</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="color">
+                <td class="td">eeee</td>
+                <td class="td">44444</td>
+                <td class="td">fffff</td>
+                <td class="td">gggggg</td>
+            </tr>
+        </tbody>
+    </table>
+    <div class="page-break">
+    </div>
+    <div>
+        @foreach(@$attachments as $attachment)
         <img class="image_style" width="100%" src="data:image/jpeg;base64,{{ base64_encode(file_get_contents(public_path($file_path . $attachment['file_name']))) }}" alt="Image">
-        @endif
-    @endforeach
+        @endforeach
+    </div>
 </body>
 
 </html>
