9e3ecfde29f9fd5d48c1c4e076b9afabfcf6dd27
Hamza Aslam <imhamzaaslam@gmail.com>
2023-08-15 12:57:37 +0500

    test

--- resources/views/layouts/pdf/template.blade.php
+++ resources/views/layouts/pdf/template.blade.php
@@ -1,19 +1,5 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>{{$downloaded_file_name}}</title>
-</head>
-
-<body>
-
-    <div>
-        @foreach (@$attachments as $attachment)
-            <img class="image_style" width="100%"
-                src="data:image/jpeg;base64,{{ base64_encode(file_get_contents(public_path($file_path . $attachment['file_name']))) }}"
-                alt="Image">
-        @endforeach
-    </div>
-</body>
-
-</html>
+@foreach (@$attachments as $attachment)
+    <img class="image_style" width="100%"
+        src="data:image/jpeg;base64,{{ base64_encode(file_get_contents(public_path($file_path . $attachment['file_name']))) }}"
+        alt="Image">
+@endforeach
